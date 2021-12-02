c44e6dcc07a84145e6b37ac994774a0b77b9f44a
coviccinelle <thi-phng@student.42.fr>
2021-12-02 11:55:34 +0100

    Done norme .h

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/07 15:09:12 by thi-phng          #+#    #+#             */
-/*   Updated: 2021/12/02 11:32:17 by thi-phng         ###   ########.fr       */
+/*   Updated: 2021/12/02 11:54:54 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 # include <stdio.h>
 # include <stdlib.h>
 
-typedef struct	s_stack
+typedef struct s_stack
 {
 	int				number;
 	struct s_stack	*next;
@@ -29,44 +29,37 @@ typedef struct	s_stack
 	int				size;
 }				t_stack;
 
-typedef struct	s_env
+typedef struct s_env
 {
 	t_stack		*stack_a;
 	t_stack		*stack_b;
 }				t_env;
 
 // UTILS //
-int	find_me(char c, const char *str);
-int	ft_int_exist(char *str);
-int	ft_all_blank(char *str);
-int	ft_atoi_1(char *str);
-int	ft_strlen(char *str);
-int	ft_invalid_char(char *str);
-int	ft_smaller(int a, int b);
-int	ft_bigger(int a, int b);
-int	ft_2nb_stick(char *str);
+int		find_me(char c, const char *str);
+int		ft_int_exist(char *str);
+int		ft_all_blank(char *str);
+int		ft_atoi_1(char *str);
+int		ft_strlen(char *str);
+int		ft_invalid_char(char *str);
+int		ft_smaller(int a, int b);
+int		ft_bigger(int a, int b);
+int		ft_2nb_stick(char *str);
 
 // PARSING PARTs //
-int	ft_parsing_1(char *str, t_stack **stack_a);
-int	ft_parsing_multi_2(int ac, char **av, t_stack **stack_a);
+int		ft_parsing_1(char *str, t_stack **stack_a);
+int		ft_parsing_multi_2(int ac, char **av, t_stack **stack_a);
 
 // Free functions
 void	ft_free_1st(t_stack **st, int n);
 void	ft_free(t_stack **st_a, t_stack **st_b, int n);
 
-
-// LISTE CHAINEE starts here //
+// LISTE CHAINEE manipulating tools //
 int		ft_new_element(t_stack	**stack, int n);
 int		ft_size_stack(t_stack **stack);
 void	ft_display_stack(t_stack *stack);
 int		ft_doublons(t_stack *stack);
 void	ft_init_elem(t_stack *new);
-
-// tools in algo
-
-void	ft_init(t_env *f);
-void	show_stay(t_stack **st);
-
 
 //       -----  OPERATIONS FUNCTIONS -----       //
 void	ft_swap(t_stack **stack, char c);
@@ -81,8 +74,8 @@ int		ft_rr_ro(t_stack **stack);
 void	ft_rrr(t_stack **stack_a, t_stack **stack_b);
 // Reverse Rotate a and b
 
-
 // big_algo_1
+void	ft_init(t_env *f);
 int		ft_sorted(t_stack **stack_a);
 void	ft_index(t_stack *stack_a);
 void	sorter(t_stack **st_a, t_stack **st_b);
@@ -94,7 +87,6 @@ t_stack	*get_p_number(t_stack **st_a, int n);
 int		get_num_on_top(t_stack **stack_a, int index, int min_steps, char c);
 int		ft_min_steps(t_stack *stack, int index);
 int		done_push_in_b(t_stack *st);
-
 
 // big_algo_2
 t_stack	*ft_max(t_stack *stack);
@@ -111,6 +103,5 @@ void	ft_algo_3(t_stack **stack);
 void	ft_algo_4(t_stack **stack_a, t_stack **stack_b);
 void	ft_algo_5(t_stack **stack_a, t_stack **stack_b);
 void	ft_algo_big(t_stack **stack_a, t_stack **stack_b);
-
 
 #endif
