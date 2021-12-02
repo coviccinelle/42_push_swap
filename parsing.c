11fd8bfaf1ed8231fbcb11f62160d6f13c63b8dd
thi-phng <thi-phng@student.42.fr>
2021-12-02 18:28:40 +0100

    Done

--- parsing.c
+++ parsing.c
@@ -6,18 +6,28 @@
 /*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 16:38:27 by thi-phng          #+#    #+#             */
-/*   Updated: 2021/12/02 17:48:39 by thi-phng         ###   ########.fr       */
+/*   Updated: 2021/12/02 18:27:19 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	invalid_min_max_int(long n, t_stack **st_a)
+int	invalid_min_max_int(long n, t_stack **st_a, int i)
 {
-	if (n == -2147483649)
+	if (i == 1)
 	{
-		ft_free_1st(st_a, 1);
-		return (1);
+		if (n == -2147483649)
+		{
+			ft_free_1st(st_a, 1);
+			return (1);
+		}
+		return (0);
+	}
+	else if (i == 0)
+	{
+		if (ft_doublons(*st_a))
+			ft_free_1st(st_a, 1);
+		return (0);
 	}
 	return (0);
 }
@@ -35,20 +45,18 @@ int	ft_parsing_1(char *str, t_stack **stack_a, int i)
 	while (str[i] && (i <= ft_strlen(str)))
 	{
 		n = ft_atoi_1(&str[i]);
-		if (invalid_min_max_int(n, stack_a))
+		if (invalid_min_max_int(n, stack_a, 1))
 			return (0);
-		ft_new_element(stack_a, n);
+		if (!ft_new_element(stack_a, n))
+			return (0);
 		while (find_me(str[i], "-0123456789"))
 			i++;
 		if (str[i] == ' ' && !ft_int_exist(&str[i]))
 			break ;
 		i++;
 	}
-	if (ft_doublons(*stack_a))
-	{
-		ft_free_1st(stack_a, 1);
+	if (invalid_min_max_int(n, stack_a, 0))
 		return (0);
-	}
 	return (1);
 }
 
@@ -80,7 +88,8 @@ int	ft_parsing_multi_2(int ac, char **av, t_stack **stack_a, int i)
 				ft_free_1st(stack_a, 1);
 				return (0);
 			}
-			ft_new_element(stack_a, n);
+			if (!ft_new_element(stack_a, n))
+				return (0);
 		}
 		i++;
 	}
